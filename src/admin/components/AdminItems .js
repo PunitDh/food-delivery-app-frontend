89744c3fd5403977e8c20205a8a73b81db8b0d57
PrateekKhindri <prateek.khindri90@gmail.com>
2022-10-03 14:04:42 +1100

    FDA-026-Created ItemForm [Prateek]

--- src/admin/components/AdminItems .js
+++ src/admin/components/AdminItems .js
@@ -1,7 +1,78 @@
-import React from "react";
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import AdminCreateItemForm from "./AdminCreateItemForm";
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
 
 const AdminItems = () => {
-  return <div>AdminItems</div>;
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_BACKEND_URL}/admin/items`)
+      .then((response) => {
+        console.log(response.data);
+        setItems(response.data);
+      });
+  }, []);
+  return (
+    <div>
+      <TableContainer component={Paper}>
+        <Table aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell align="left">name</StyledTableCell>
+              <StyledTableCell align="left">category</StyledTableCell>
+              <StyledTableCell align="left">image</StyledTableCell>
+              <StyledTableCell align="left">rating</StyledTableCell>
+              <StyledTableCell align="left">rating</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {items?.map((row) => (
+              <StyledTableRow key={row.id}>
+                <StyledTableCell align="left">{row.name}</StyledTableCell>
+                <StyledTableCell align="left">{row.category}</StyledTableCell>
+                <StyledTableCell align="left">
+                  <img src={row.image} alt="" />
+                </StyledTableCell>
+                <StyledTableCell align="left">{row.price}</StyledTableCell>
+                <StyledTableCell align="left">{row.rating}</StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <AdminCreateItemForm />
+    </div>
+  );
 };
 
 export default AdminItems;
